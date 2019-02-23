59ee6639f99affbc615823e339bbd959eebcce90
derry <spann_derry@bah.com>
2019-02-23 17:00:39 -0500

    something up with images

--- src/js/views/mainView.js
+++ src/js/views/mainView.js
@@ -1,18 +1,19 @@
 import { h, app } from "hyperapp"
 import Regions from "../data/Regions"
+import thumbView from "./thumbView";
 
-const smset = Regions.slice(0,4);
+const smset = Regions.slice(0, 4);
 export default (columndata) => {
     return (<div class="main-page grid-layout four-column">
-        {smset.map((region, i)=>{
+        {smset.map((region, i) => {
             return (
                 <div class="col">
-                    <div class="section-content"></div>
+                    <div class="section-content">{columndata.map((plant)=>thumbView(plant))}</div>
                     <div class="section-title">{region}</div>
                 </div>
             )
         })}
-        
+
     </div>
     )
 };
