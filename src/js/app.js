4f468c47a78bb77ff91ab06d4b15e0e66f1df31a
derry <spann_derry@bah.com>
2019-02-23 15:13:33 -0500

    added ben cards and reduced json data

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ import { h, app } from "hyperapp"
 
 var Plants = require("./data/plant.json");
 /** @jsx h */
+var BadTypes = "Hydro,Gas,Other,Oil,Nuclear,Coal,Waste,\
+Biomass,Geothermal,NA,Cogeneration,Storage,Petcoke"
+.split(',');
 
 const state = {
     plants: Plants,
@@ -26,9 +29,68 @@ const card = (item) => {
         )
 };
 
+const bcard = (item) => {
+    return (
+        <div class="card">
+            <div class="card-header">
+                <div class="country-image">
+                    <img src="images/country/usa.png" />
+                </div>
+                <div class="card-title">
+                        <div class="card-maintitle">{item.name}</div>
+                    <div class="card-subtitle">{item.country_long}</div>
+                </div>
+            </div>
+            <div class={'card-content content-'+(BadTypes.indexOf(item.fuel1)== -1?'good':'bad')}>
+                <div class="card-year">2016</div>
+                <div class="card-icon">
+                        <img src={'images/type/'+item.fuel1+'.png'} />
+                </div>
+            </div>
+            <div class="card-footer">
+                <div class="footer-label">
+                    578 gwh generated
+                </div>
+                <div class="generation-viz">
+                    <div class="unit-on"></div>
+                    <div class="unit-on"></div>
+                    <div class="unit-on"></div>
+                    <div class="unit-on"></div>
+                    <div class="unit-on"></div>
+                    <div class="unit-on"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                    <div class="unit-off"></div>
+                </div>
+            </div>
+        </div>
+        )
+}
+
 const view = (state, actions) => (
-    <div class="row grid1">
-        {state.plants.map((plant,i)=> card(plant) )}
+    <div class="row grid-layout">
+        {state.plants.map((plant,i)=> bcard(plant) )}
     </div>
 )
 console.log(Plants);
